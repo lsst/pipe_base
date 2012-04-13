b3511d2fbe1dd2db058a283b6bcb0d9bd6016fe0
Russell Owen <rowen@uw.edu>
2012-04-13 16:21:52 -0500

    Fix a typo in a comment

--- python/lsst/pipe/base/cmdLineTask.py
+++ python/lsst/pipe/base/cmdLineTask.py
@@ -46,7 +46,7 @@ class CmdLineTask(Task):
     def parseAndRun(cls, args=None, config=None, log=None):
         """Parse an argument list and run the command
 
-        @param args: list of command-line arguments; if None use sys.arv
+        @param args: list of command-line arguments; if None use sys.argv
         @param config: config for task (instance of pex_config Config); if None use cls.ConfigClass()
         @param log: log (instance of pex_logging Log); if None use the default log
         
