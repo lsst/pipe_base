ec956e6736d78491e198a103b2f43fe825a63b1e
Jim Bosch <jbosch@astro.princeton.edu>
2012-07-28 12:28:38 +0900

    attach parsed command object to command-line task task instance

--- python/lsst/pipe/base/cmdLineTask.py
+++ python/lsst/pipe/base/cmdLineTask.py
@@ -47,12 +47,15 @@ class CmdLineTask(Task):
         - parsedCmd: the parsed command returned by argumentParser.parse_args
         - task: the instantiated task
         The return values are primarily for testing and debugging
+        
+        The parsedCmd object is also attached as an instance variable of the task.
         """
         argumentParser = cls._makeArgumentParser()
         if config is None:
             config = cls.ConfigClass()
         parsedCmd = argumentParser.parse_args(config=config, args=args, log=log)
         task = cls(name = cls._DefaultName, config = parsedCmd.config, log = parsedCmd.log)
+        task.parsedCmd = parsedCmd
         task._runParsedCmd(parsedCmd)
         return Struct(
             argumentParser = argumentParser,
