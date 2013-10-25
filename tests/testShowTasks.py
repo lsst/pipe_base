391878ca6f13c40919db29b1aced51ed79e6b3a5
Russell Owen <rowen@uw.edu>
2013-10-25 10:45:09 -0700

    Correct copyright date in new test

--- tests/testShowTasks.py
+++ tests/testShowTasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # 
 # LSST Data Management System
-# Copyright 2008, 2009, 2010 LSST Corporation.
+# Copyright 2013 LSST Corporation.
 # 
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
