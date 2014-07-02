6c2ac3cdabb91828156eef074aabcf8f7503c9b0
Robert Lupton the Good <rhl@astro.princeton.edu>
2014-07-02 12:01:25 -1000

    Implemented --show glob=PATTERN; DM-939

--- python/lsst/pipe/base/argumentParser.py
+++ python/lsst/pipe/base/argumentParser.py
@@ -21,6 +21,7 @@
 #
 import argparse
 import collections
+import fnmatch
 import itertools
 import os
 import re
@@ -243,7 +244,7 @@ class ArgumentParser(argparse.ArgumentParser):
         self.add_argument("--doraise", action="store_true",
             help="raise an exception on error (else log a message and continue)?")
         self.add_argument("--logdest", help="logging destination")
-        self.add_argument("--show", nargs="+", choices="config data tasks run".split(), default=(),
+        self.add_argument("--show", nargs="+", default=(),
             help="display the specified information to stdout and quit (unless run is specified).")
         self.add_argument("-j", "--processes", type=int, default=1, help="Number of processes to use")
         self.add_argument("--clobber-output", action="store_true", dest="clobberOutput", default=False,
@@ -376,11 +377,7 @@ class ArgumentParser(argparse.ArgumentParser):
         namespace.log.info("calib=%s"  % (namespace.calib,))
         namespace.log.info("output=%s" % (namespace.output,))
         
-        if "tasks" in namespace.show:
-            showTaskHierarchy(config=namespace.config)
-        
-        if "config" in namespace.show:
-            namespace.config.saveToStream(sys.stdout, "config")
+        self._obeyShowArgument(namespace.show, namespace.config, exit=False)
 
         namespace.butler = dafPersist.Butler(
             root = namespace.input,
@@ -429,6 +426,53 @@ class ArgumentParser(argparse.ArgumentParser):
 
         return namespace
     
+    def _obeyShowArgument(self, showOpts, config=None, exit=False):
+        """!Process arguments specified with --show
+        \param showOpts  List of options passed to --show
+        \param config    The provided config
+        \param exit      Exit if "run" isn't included in showOpts
+
+        - config     Dump all the config entries
+        - data       Ignored; to be processed by caller
+        - glob=pat   Dump all the config entries that match PAT
+        - run        Keep going (the default behaviour is to exit if --show is specified)
+        - tasks      Show task hierarchy
+        """
+        if not showOpts:
+            return
+
+        for what in showOpts:
+            if what == "config":
+                config.saveToStream(sys.stdout, "config")
+            elif what == "data":
+                pass
+            elif fnmatch.fnmatch(what, "glob=*"):
+                class FilteredStream(object):
+                    """A file object that only prints lines that match the glob "pattern"
+
+                    N.b. Newlines are silently discarded and reinserted;  crude but effective.
+                    """
+                    def __init__(self, pattern):
+                        self._pattern = pattern
+
+                    def write(self, str):
+                        str = str.rstrip()
+                        if str and fnmatch.fnmatch(str, self._pattern):
+                            print str
+
+                config.saveToStream(FilteredStream(what[5:]), "config")
+            elif what == "run":
+                pass
+            elif what == "tasks":
+                showTaskHierarchy(config)
+            else:
+                print >> sys.stderr, "Unknown value for show: %s (choose from '%s')" % \
+                    (what, "', '".join("config glob=XXX tasks run".split()))
+                sys.exit(1)
+
+        if exit and "run" not in showOpts:
+            sys.exit(0)
+
     def _processDataIds(self, namespace):
         """Process the parsed data for each data ID argument
         
