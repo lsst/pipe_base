8ea3ec13320881dd0d847792de18061a53073e5a
Jonathan Sick <jsick@lsst.org>
2017-07-29 23:19:16 -0400

    Convert struct module docstrings to Numpydoc

--- python/lsst/pipe/base/struct.py
+++ python/lsst/pipe/base/struct.py
@@ -27,10 +27,17 @@ __all__ = ["Struct"]
 
 
 class Struct(object):
-    """!A struct to which you can add any fields
+    """A container to which you can add fields as attributes.
 
-    Intended to be used for the return value from Task.run and other Task methods,
-    and useful for any method that returns multiple values.
+    Parameters
+    ----------
+    keyArgs
+        keyword arguments specifying fields and their values.
+
+    Notes
+    -----
+    Intended to be used for the return value from `~lsst.pipe.base.Task.run` and other `~lsst.pipe.base.Task`
+    methods, and useful for any method that returns multiple values.
 
     The intent is to allow accessing returned items by name, instead of unpacking a tuple.
     This makes the code much more robust and easier to read. It allows one to change what values are returned
@@ -38,38 +45,41 @@ class Struct(object):
     causes errors that are caught quickly and reported in a way that is easy to understand.
 
     The primary reason for using Struct instead of dict is that the fields may be accessed as attributes,
-    e.g. aStruct.foo instead of aDict["foo"]. Admittedly this only saves a few characters, but it makes
-    the code significantly more readable.
+    e.g. ``aStruct.foo`` instead of ``aDict["foo"]``. Admittedly this only saves a few characters, but it
+    makes the code significantly more readable.
 
     Struct is preferred over named tuples, because named tuples can be used as ordinary tuples, thus losing
     all the safety advantages of Struct. In addition, named tuples are clumsy to define and Structs
     are much more mutable (e.g. one can trivially combine Structs and add additional fields).
+
+    Examples
+    --------
+    >>> myStruct = Struct(
+    >>>     strVal = 'the value of the field named "strVal"',
+    >>>     intVal = 35,
+    >>> )
+
     """
 
     def __init__(self, **keyArgs):
-        """!Create a Struct with the specified field names and values
-
-        For example:
-        @code
-        myStruct = Struct(
-            strVal = 'the value of the field named "strVal"',
-            intVal = 35,
-        )
-        @endcode
-
-        @param[in] **keyArgs    keyword arguments specifying name=value pairs
-        """
         object.__init__(self)
         for name, val in keyArgs.items():
             self.__safeAdd(name, val)
 
     def __safeAdd(self, name, val):
-        """!Add a field if it does not already exist and name does not start with __ (two underscores)
+        """Add a field if it does not already exist and name does not start with ``__`` (two underscores).
 
-        @param[in] name name of field to add
-        @param[in] val  value of field to add
+        Parameters
+        ----------
+        name : `str`
+            Name of field to add.
+        val : object
+            Value of field to add.
 
-        @throw RuntimeError if name already exists or starts with __ (two underscores)
+        Raises
+        ------
+        RuntimeError
+            Raised if name already exists or starts with ``__`` (two underscores).
         """
         if hasattr(self, name):
             raise RuntimeError("Item %s already exists" % (name,))
@@ -78,29 +88,49 @@ class Struct(object):
         setattr(self, name, val)
 
     def getDict(self):
-        """!Return a dictionary of attribute name: value
+        """Get a dictionary of fields in this struct.
 
-        @warning: the values are shallow copies.
+        Returns
+        -------
+        structDict : `dict`
+            Dictionary with field names as keys and field values as values. The values are shallow copies.
         """
         return self.__dict__.copy()
 
     def mergeItems(self, struct, *nameList):
-        """!Copy specified fields from another struct, provided they don't already exist
+        """Copy specified fields from another struct, provided they don't already exist.
 
-        @param[in] struct       struct from which to copy
-        @param[in] *nameList    all remaining arguments are names of items to copy
+        Parameters
+        ----------
+        struct : `Struct`
+            `Struct` from which to copy.
+        *nameList : `str`
+            All remaining arguments are names of items to copy.
 
-        For example: foo.copyItems(other, "itemName1", "itemName2")
-        copies other.itemName1 and other.itemName2 into self.
+        Raises
+        ------
+        RuntimeError
+            Raised if any item in nameList already exists in self (but any items before the conflicting item
+            in nameList will have been copied).
 
-        @throw RuntimeError if any item in nameList already exists in self
-            (but any items before the conflicting item in nameList will have been copied)
+        Examples
+        --------
+        For example::
+
+            foo.copyItems(other, "itemName1", "itemName2")
+
+        copies ``other.itemName1`` and ``other.itemName2`` into self.
         """
         for name in nameList:
             self.__safeAdd(name, getattr(struct, name))
 
     def copy(self):
-        """!Return a one-level-deep copy (values are not copied)
+        """Make a one-level-deep copy (values are not copied).
+
+        Returns
+        -------
+        copy : `Struct`
+            One-level-deep copy of this Struct.
         """
         return Struct(**self.getDict())
 
