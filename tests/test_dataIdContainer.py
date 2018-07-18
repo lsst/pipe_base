c4c3636520c9eb4b892bcd4bbf5c6e47cbf605f9
John K. Parejko <parejkoj@uw.edu>
2018-07-18 13:20:14 -0700

    Add tests of DataIdContainer and improve exception handling

---
+++ tests/test_dataIdContainer.py
@@ -0,0 +1,66 @@
+# This file is part of pipe_base.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Tests of the DataIdContainer class."""
+
+import unittest
+import unittest.mock
+
+import lsst.daf.persistence
+import lsst.utils.tests
+import lsst.pipe.base as pipeBase
+
+
+class DataIdContainerTestCase(lsst.utils.tests.TestCase):
+    def setUp(self):
+        self.container = pipeBase.DataIdContainer()
+        self.butler = unittest.mock.MagicMock(spec=lsst.daf.persistence.Butler)
+
+    def test_castDataIdsNoneDatasetType(self):
+        """Raise RuntimeError if we haven't set container.datasetType."""
+        with self.assertRaises(RuntimeError):
+            self.container.castDataIds(self.butler)
+
+    def test_makeDataRefListNoneDatasetType(self):
+        """Raise RuntimeError if we haven't set container.datasetType."""
+        with self.assertRaises(RuntimeError):
+            self.container.makeDataRefList(self.butler)
+
+    def test_castDataIdsRaiseKeyError(self):
+        """Test that castDataIds re-raises a butler.getKeys() exception."""
+        self.container.setDatasetType("nonsense!")
+        self.butler.getKeys.side_effect = KeyError
+        with self.assertRaises(KeyError) as cm:
+            self.container.castDataIds(self.butler)
+        self.assertIsInstance(cm.exception.__cause__, KeyError)
+
+
+class MyMemoryTestCase(lsst.utils.tests.MemoryTestCase):
+    pass
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
+if __name__ == "__main__":
+    lsst.utils.tests.init()
+    unittest.main()
