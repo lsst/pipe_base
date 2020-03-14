d1453e0f7584b45c67675ce337275f3b155a4e42
Tim Jenness <tjenness@lsst.org>
2020-03-14 09:58:37 -0700

    Allow the dataset type to not be defined yet
    We may be wanting to register it later.

--- python/lsst/pipe/base/pipeline.py
+++ python/lsst/pipe/base/pipeline.py
@@ -477,7 +477,10 @@ class TaskDatasetTypes:
                 else:
                     datasetType = DatasetType(c.name, registry.dimensions.extract(dimensions),
                                               c.storageClass)
-                    registryDatasetType = registry.getDatasetType(c.name)
+                    try:
+                        registryDatasetType = registry.getDatasetType(c.name)
+                    except KeyError:
+                        registryDatasetType = datasetType
                     if datasetType != registryDatasetType:
                         raise ValueError(f"Supplied dataset type ({datasetType}) inconsistent with "
                                          f"registry definition ({registryDatasetType})")
