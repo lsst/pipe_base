3c954b0eb1fac575eec78b73ef27699d29093bea
Tim Jenness <tjenness@lsst.org>
2021-07-26 15:28:00 -0700

    Export TaskLogAdapter and getLogger by default

--- python/lsst/pipe/base/task_logging.py
+++ python/lsst/pipe/base/task_logging.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ("TRACE", "VERBOSE", "getLogger")
+__all__ = ("TRACE", "VERBOSE", "getLogger", "TaskLogAdapter")
 
 import logging
 from logging import LoggerAdapter
