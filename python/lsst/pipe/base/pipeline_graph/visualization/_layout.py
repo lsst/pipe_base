be8168bd223d493547918f643375ab3e63a2c20c
Jim Bosch <jbosch@astro.princeton.edu>
2023-03-16 10:10:01 -0400

    WIP: new layout algorithm that ranks unblocked nodes.

--- python/lsst/pipe/base/pipeline_graph/visualization/_layout.py
+++ python/lsst/pipe/base/pipeline_graph/visualization/_layout.py
@@ -24,7 +24,8 @@ __all__ = ("Layout",)
 
 import dataclasses
 import itertools
-from collections.abc import Iterator
+from collections import defaultdict
+from collections.abc import Iterable, Iterator
 from typing import Generic, TextIO, TypeVar
 
 import networkx
@@ -47,71 +48,66 @@ class LayoutRow(Generic[_K]):
 class Layout(Generic[_K]):
     def __init__(self, graph: networkx.DiGraph):
         self._graph = graph
-        self._todo_graph = graph.copy()
         self._active_columns: dict[int, set[_K]] = {}
         self._locations: dict[_K, int] = {}
+        self._last_x = 0
         self.x_max = 0
         for component in list(networkx.algorithms.components.weakly_connected_components(graph)):
             self._add_connected_graph(graph.subgraph(component))
-        assert not self._todo_graph, list(self._todo_graph)
-        del self._todo_graph
         del self._active_columns
+        del self._last_x
 
-    def _add_unblocked_node(self, node: _K) -> int:
-        assert self._todo_graph.in_degree(node) == 0, str(node)
+    def _compute_closing_rank(self, node: _K) -> int:
+        rank = 2 * self._graph.out_degree(node)
+        for destinations in self._active_columns.values():
+            if node in destinations:
+                rank -= 2
+        # Small bonus to rank if this ends an edge in the current column.
+        # if node in self._active_columns.get(self._last_x, frozenset()):
+        #     rank -= 1
+        return rank
+
+    def _add_unblocked_node(self, node: _K) -> set[_K]:
+        node_x = None
         for active_column_x, active_column_endpoints in list(self._active_columns.items()):
             if node in active_column_endpoints:
                 active_column_endpoints.remove(node)
                 if not active_column_endpoints:
                     del self._active_columns[active_column_x]
-        for node_x in itertools.count():
-            if node_x not in self._active_columns:
-                break
-        outgoing = set(self._todo_graph.successors(node))
+                    if node_x is None:
+                        node_x = active_column_x
+        if node_x is None:
+            for node_x in itertools.count():
+                if node_x not in self._active_columns:
+                    break
+        outgoing = set(self._graph.successors(node))
         self._locations[node] = node_x
         self.x_max = max(node_x, self.x_max)
-        self._todo_graph.remove_node(node)
+        self._last_x = node_x
         if outgoing:
             self._active_columns[node_x] = outgoing
-        return node_x
+        return outgoing
 
-    def _add_active_unblocked(self, avoid: _K) -> None:
-        while True:
-            # First we immediately add any nodes that don't have outgoing
-            # edges (since these won't occupy new columns for more than a
-            # single row), and remember the rest that are unblocked.
-            unblocked = set()
-            for node in list(itertools.chain.from_iterable(self._active_columns.values())):
-                if node != avoid and node in self._todo_graph and self._todo_graph.in_degree(node) == 0:
-                    if self._todo_graph.out_degree(node) == 0:
-                        self._add_unblocked_node(node)
-                    else:
-                        unblocked.add(node)
-            if not unblocked:
-                return
-            # Add the unblocked nodes that do have outgoing edges, starting
-            # with those that have the fewest outgoing edges, while recursing
-            # to include the nodes those unblock.
-            for node in sorted(unblocked, key=lambda n: self._todo_graph.out_degree(n), reverse=True):
-                self._add_unblocked_node(node)
+    def _add_until_successors(self, nodes: Iterable[_K], unblocked: set[_K]) -> bool:
+        for node in nodes:
+            successors = self._add_unblocked_node(node)
+            unblocked.remove(node)
+            for successor in successors:
+                if all(n in self._locations for n in self._graph.predecessors(successor)):
+                    unblocked.add(successor)
+            if successors:
+                return False
+        return True
 
     def _add_connected_graph(self, xgraph: networkx.DiGraph) -> None:
-        for node in networkx.algorithms.dag.dag_longest_path(xgraph):
-            # Terminate all active edges that are now unblocked, continuing
-            # until there are none unblocked other than node.
-            self._add_active_unblocked(node)
-            ancestors = list(networkx.algorithms.dag.ancestors(self._todo_graph, node))
-            if ancestors:
-                # Recurse to actually add this node and all of its remaining
-                # ancestors to the graph.  Including this node in the subgraph
-                # ensures that it's connected.
-                ancestors.append(node)
-                self._add_connected_graph(self._todo_graph.subgraph(ancestors))
-            else:
-                self._add_unblocked_node(node)
-        # There can't be any remaining nodes in xgraph that had been blocked by
-        # the last node, since that would have made them part of the longest
-        # path.
+        unblocked = set(next(networkx.algorithms.dag.topological_generations(xgraph)))
+        while unblocked:
+            unblocked_by_rank = defaultdict(set)
+            for node in unblocked:
+                unblocked_by_rank[self._compute_closing_rank(node)].add(node)
+            for rank in sorted(unblocked_by_rank):
+                if not self._add_until_successors(unblocked_by_rank[rank], unblocked):
+                    break
 
     def print(self, stream: TextIO) -> None:
         for row in self:
