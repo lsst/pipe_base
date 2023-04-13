d3b240a48cba0a76cfb80135946ff91b15af1a9b
Krzysztof Findeisen <kfindeis@uw.edu>
2023-04-13 14:34:10 -0700

    Reformat QuantumGraph property docs.
    The docs have been brought in line with current DM standards, and an
    intersphinx link has been added for networkx types.

--- doc/conf.py
+++ doc/conf.py
@@ -6,3 +6,5 @@ html_title = project
 html_short_title = project
 doxylink = {}
 exclude_patterns = ["changes/*"]
+
+intersphinx_mapping['networkx'] = ('https://networkx.org/documentation/stable/', None)  # noqa: F405
