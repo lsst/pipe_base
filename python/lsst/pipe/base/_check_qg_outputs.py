7ca9897dac1dbdb1f0b23c66f9bdeda929011e5c
Orion Eiger <eiger@slac.stanford.edu>
2023-07-26 13:17:38 -0700

    Add failed_upstream, improve bookkeeping, add strings

--- python/lsst/pipe/base/_check_qg_outputs.py
+++ python/lsst/pipe/base/_check_qg_outputs.py
@@ -25,6 +25,7 @@ import itertools
 import uuid
 from collections.abc import Iterable, Iterator
 
+import networkx
 from lsst.daf.butler import Butler, DataCoordinate, DatasetRef
 from lsst.resources import ResourcePath, ResourcePathExpression
 
@@ -35,27 +36,54 @@ from .pipeline import PipelineDatasetTypes
 @dataclasses.dataclass
 class TaskExecutionReport:
     failed: set[DataCoordinate] = dataclasses.field(default_factory=set)
+    failed_upstream: set[DataCoordinate] = dataclasses.field(default_factory=set)
     missing_datasets_failed: set[DatasetRef] = dataclasses.field(default_factory=set)
-    missing_datasets_not_produced: set[DatasetRef] = dataclasses.field(default_factory=set)
+    missing_datasets_not_produced: dict[DatasetRef, bool] = dataclasses.field(default_factory=dict)
+    # bool: predicted inputs to this task were not produced
+    missing_upstream_failed: set[DatasetRef] = dataclasses.field(default_factory=set)
     datasets_produced: dict[str, int] = dataclasses.field(default_factory=dict)
 
-    def inspect_quantum(self, quantum, refs, metadata_name):
+    def inspect_quantum(self, quantum_node, status_graph: networkx.DiGraph, refs, metadata_name):
+        quantum = quantum_node.quantum
         (ref,) = quantum.outputs[metadata_name]
         if ref.id not in refs[metadata_name]:
-            self.failed.add(quantum.dataId)
+            if any(
+                status_graph.nodes[upstream_quantum_id]["failed"]
+                for upstream_dataset_id in status_graph.predecessors(quantum_node.nodeId)
+                for upstream_quantum_id in status_graph.predecessors(upstream_dataset_id)
+            ):
+                self.failed_upstream.add(quantum.dataId)
+            else:
+                self.failed.add(quantum.dataId)
             failed = True
         else:
             failed = False
+        status_graph.nodes[quantum_node.nodeId]["failed"] = failed
         for ref in itertools.chain.from_iterable(quantum.outputs.values()):
             if ref.id not in refs[ref.datasetType.name]:
                 if failed:
-                    self.missing_datasets_failed.add(ref)
+                    for upstream_quantum_id in status_graph.predecessors(ref.id):
+                        if status_graph.nodes[upstream_quantum_id]["failed"]:
+                            self.missing_upstream_failed.add(ref)
+                            break
+                    else:
+                        self.missing_datasets_failed.add(ref)
                 else:
-                    self.missing_datasets_not_produced.add(ref)
+                    status_graph.nodes[ref.id]["not_produced"] = True
+                    self.missing_datasets_not_produced[ref] = any(
+                        status_graph.nodes[upstream_dataset_id].get("not_produced", False)
+                        for upstream_quantum_id in status_graph.predecessors(ref.id)
+                        for upstream_dataset_id in status_graph.predecessors(upstream_quantum_id)
+                    )
+
             else:
+                status_graph.nodes[ref.id]["not_produced"] = False
                 self.datasets_produced[ref.datasetType.name] = (
                     self.datasets_produced.setdefault(ref.datasetType.name, 0) + 1
                 )
+
+    def __str__(self) -> str:
+        return f"failed: {len(self.failed)}\nfailed upstream: {len(self.failed_upstream)}\n"
 
 
 @dataclasses.dataclass
@@ -72,6 +100,7 @@ class QuantumGraphExecutionReport:
     ) -> Iterator[QuantumGraphExecutionReport]:
         pipeline_dataset_types = None
         refs = {}
+        status_graph = networkx.DiGraph()
         for graph_uri in graph_uris:
             graph_uri = ResourcePath(graph_uri)
             qg = QuantumGraph.loadUri(graph_uri)
@@ -94,11 +123,24 @@ class QuantumGraphExecutionReport:
                         )
                     }
             for taskDef in qg.iterTaskGraph():
+                for node in qg.getNodesForTask(taskDef):
+                    status_graph.add_node(node.nodeId)
+                    for ref in itertools.chain.from_iterable(node.quantum.outputs.values()):
+                        status_graph.add_edge(node.nodeId, ref.id)
+                    for ref in itertools.chain.from_iterable(node.quantum.inputs.values()):
+                        status_graph.add_edge(ref.id, node.nodeId)
+
+            for taskDef in qg.iterTaskGraph():
                 task_report = TaskExecutionReport()
                 for node in qg.getNodesForTask(taskDef):
-                    task_report.inspect_quantum(node.quantum, refs, metadata_name=taskDef.metadataDatasetName)
+                    task_report.inspect_quantum(
+                        node, status_graph, refs, metadata_name=taskDef.metadataDatasetName
+                    )
                 report.tasks[taskDef.label] = task_report
             yield report
+
+    def __str__(self) -> str:
+        return "\n".join(f"{tasklabel}:{report}" for tasklabel, report in self.tasks.items())
 
 
 def lookup_quantum_dataId(graph_uri: ResourcePathExpression, nodes: Iterable[uuid.UUID]):
