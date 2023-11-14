876c7c8e04a49841bb55d9041d91fe1f966464ea
Orion Eiger <eiger@slac.stanford.edu>
2023-11-14 13:45:22 -0800

    Add new graphs to combined provenance graph

---
+++ python/lsst/pipe/base/quantum_provenance_graph.py
@@ -0,0 +1,147 @@
+# This file is part of pipe_base.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This software is dual licensed under the GNU General Public License and also
+# under a 3-clause BSD license. Recipients may choose which of these licenses
+# to use; please see the files gpl-3.0.txt and/or bsd_license.txt,
+# respectively.  If you choose the GPL option then the following text applies
+# (but note that there is still no warranty even if you opt for BSD instead):
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""An under-construction version of QuantumGraph and various helper
+classes.
+"""
+
+from __future__ import annotations
+
+__all__ = (
+    "QuantumProvenanceGraph",
+    "QuantumKey",
+    "DatasetKey",
+    "PrerequisiteDatasetKey",
+)
+
+import itertools
+from typing import TYPE_CHECKING, ClassVar, Literal, NamedTuple
+
+import networkx
+from lsst.daf.butler import Butler, DataIdValue
+from lsst.resources import ResourcePathExpression
+from lsst.utils.logging import getLogger
+
+from .graph import QuantumGraph
+
+if TYPE_CHECKING:
+    pass
+
+_LOG = getLogger(__name__)
+
+
+class QuantumKey(NamedTuple):
+    """Identifier type for quantum keys in a `QuantumProvenanceGraph`."""
+
+    task_label: str
+    """Label of the task in the pipeline."""
+
+    data_id_values: tuple[DataIdValue, ...]
+    """Data ID values of the quantum.
+
+    Note that keys are fixed given `task_label`, so using only the values here
+    speeds up comparisons.
+    """
+
+    is_task: ClassVar[Literal[True]] = True
+    """Whether this node represents a quantum rather
+    than a dataset (always `True`).
+    """
+
+
+class DatasetKey(NamedTuple):
+    """Identifier type for dataset keys in a `QuantumProvenanceGraph`."""
+
+    parent_dataset_type_name: str
+    """Name of the dataset type (never a component)."""
+
+    data_id_values: tuple[DataIdValue, ...]
+    """Data ID values of the dataset.
+
+    Note that keys are fixed given `parent_dataset_type_name`, so using only
+    the values here speeds up comparisons.
+    """
+
+    is_task: ClassVar[Literal[False]] = False
+    """Whether this node represents a quantum rather
+    than a dataset (always `False`).
+    """
+
+    is_prerequisite: ClassVar[Literal[False]] = False
+
+
+class PrerequisiteDatasetKey(NamedTuple):
+    """Identifier type for prerequisite dataset keys in a
+    `QuantumProvenanceGraph`.
+
+    Unlike regular datasets, prerequisites are not actually required to come
+    from a find-first search of `input_collections`, so we don't want to
+    assume that the same data ID implies the same dataset.  Happily we also
+    don't need to search for them by data ID in the graph, so we can use the
+    dataset ID (UUID) instead.
+    """
+
+    parent_dataset_type_name: str
+    """Name of the dataset type (never a component)."""
+
+    dataset_id_bytes: bytes
+    """Dataset ID (UUID) as raw bytes."""
+
+    is_task: ClassVar[Literal[False]] = False
+    """Whether this node represents a quantum rather
+    than a dataset (always `False`).
+    """
+
+    is_prerequisite: ClassVar[Literal[True]] = True
+
+
+class QuantumProvenanceGraph:
+    """A set of already-run, merged quantum graphs with provenance
+    information.
+    """
+
+    def __init__(self):
+        self._xgraph = networkx.DiGraph()
+
+    def add_new_graph(self, qgraph: QuantumGraph | ResourcePathExpression, butler: Butler) -> None:
+        if not isinstance(qgraph, QuantumGraph):
+            qgraph = QuantumGraph.loadUri(qgraph)
+        assert qgraph.metadata is not None, "Saved QGs always have metadata."
+        qgraph.metadata["output_run"]
+        for node in qgraph:
+            quantum_key = QuantumKey(node.taskDef.label, node.quantum.dataId.values_tuple())
+            self._xgraph.add_node(quantum_key)
+            for ref in itertools.chain.from_iterable(node.quantum.outputs.values()):
+                dataset_key = DatasetKey(ref.datasetType.nameAndComponent()[0], ref.dataId.values_tuple())
+                self._xgraph.add_edge(quantum_key, dataset_key)
+            for ref in itertools.chain.from_iterable(node.quantum.inputs.values()):
+                dataset_key = DatasetKey(ref.datasetType.nameAndComponent()[0], ref.dataId.values_tuple())
+                if dataset_key in self._xgraph:
+                    self._xgraph.add_edge(dataset_key, quantum_key)
+
+    def resolve_duplicates():
+        pass
