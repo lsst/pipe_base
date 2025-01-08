05814d245c64ec5ce391815bd7de8351b23172d0
Jim Bosch <jbosch@astro.princeton.edu>
2025-01-08 16:17:27 -0500

    Silence MyPy errors about Pydantic/Sphinx workaround.
    This may well be a MyPy bug; certainly IMO more checking on function
    signatures that are already full of typing.Any is not an improvement.

--- python/lsst/pipe/base/_task_metadata.py
+++ python/lsst/pipe/base/_task_metadata.py
@@ -670,7 +670,7 @@ class TaskMetadata(BaseModel):
             return super().model_copy(*args, **kwargs)
 
         @classmethod
-        def model_construct(cls, *args: Any, **kwargs: Any) -> Any:
+        def model_construct(cls, *args: Any, **kwargs: Any) -> Any:  # type: ignore[misc]
             """See `pydantic.BaseModel.model_construct`."""
             return super().model_construct(*args, **kwargs)
 
