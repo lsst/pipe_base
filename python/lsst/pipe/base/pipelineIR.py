5f24cf95341eeb2676ce1050bfbece6a2dd18482
Jim Bosch <jbosch@astro.princeton.edu>
2025-04-13 15:30:22 -0400

    Drop empty-but-unaffected pipeline subsets in DROP mode.
    This would really be more coherent if the LabeledSubsetModify mode
    encoded what to do with empty subsets directly, but we can't do that
    without modifying a ton of pipelines, and that means we have to take
    our best guess at intent based on current usage.

--- python/lsst/pipe/base/pipelineIR.py
+++ python/lsst/pipe/base/pipelineIR.py
@@ -984,6 +984,15 @@ class PipelineIR:
                     case PipelineSubsetCtrl.EDIT:
                         for extra in extraTaskLabels:
                             labeled_subset.subset.discard(extra)
+            elif subsetCtrl is PipelineSubsetCtrl.DROP and not labeled_subset.subset:
+                # When mode is DROP, also drop any subsets that were already
+                # empty.  This ensures we drop steps that were emptied-out by
+                # (earlier) imports with exclude in EDIT mode.  Note that we
+                # don't want to drop those steps when they're first excluded
+                # down to nothing, because the pipeline might be about to add
+                # new tasks back into them, and then we'd want to preserve the
+                # step definitions.
+                del pipeline.labeled_subsets[label]
 
         # remove any steps that correspond to removed subsets
         new_steps = []
