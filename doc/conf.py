75a1efe1b81e8a07e11dbd7857b2946689e99b5b
Jim Bosch <jbosch@astro.princeton.edu>
2026-01-21 13:25:57 -0500

    Switch doc build to documenteer 2 / sphinxutils.

--- doc/conf.py
+++ doc/conf.py
@@ -1,24 +1,9 @@
-from documenteer.conf.pipelinespkg import *  # noqa: F403, import *
+"""Sphinx configuration file for an LSST stack package.
 
-project = "pipe_base"
-html_theme_options["logotext"] = project  # noqa: F405, unknown name
-html_title = project
-html_short_title = project
-doxylink = {}
-exclude_patterns = ["changes/*"]
+This configuration only affects single-package Sphinx documenation builds.
+"""
+# ruff: noqa: F403, F405
 
-intersphinx_mapping["networkx"] = ("https://networkx.org/documentation/stable/", None)  # noqa: F405
-intersphinx_mapping["lsst"] = ("https://pipelines.lsst.io/v/weekly/", None)  # noqa: F405
-intersphinx_mapping["pydantic"] = ("https://docs.pydantic.dev/latest/", None)  # noqa: F405
+from documenteer.conf.guide import *
 
-# As a temporary hack until we move to documenteer 2 delete scipy
-# (since it no longer works)
-try:
-    del intersphinx_mapping["scipy"]  # noqa: F405
-except KeyError:
-    pass
-
-nitpick_ignore = [
-    ("py:mod", "lsst.meas.algorithms.starSelectorRegistry"),
-    ("py:mod", "lsst.meas.algorithms.psfDeterminerRegistry"),
-]
+exclude_patterns.append("changes/*")
